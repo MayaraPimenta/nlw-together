ea387354e383d6d9d0d4551103c72fada93f196a
Mayara <mayaralrpimenta@gmail.com>
2021-06-23 21:32:36 -0300

    Aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 import { AuthContextProvider } from "../src/contexts/AuthContext"
+
 
 function App() {
    return (
     <AuthContextProvider> 
-    <BrowserRouter>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
-    </BrowserRouter>
+      <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/new" component={NewRoom} />
+            <Route path="/rooms/:id" component={Room} />
+          </Switch>
+      </BrowserRouter>
     </AuthContextProvider>
   );
 }
